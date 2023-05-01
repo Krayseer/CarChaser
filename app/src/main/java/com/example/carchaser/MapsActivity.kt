83a1511c28b83b7faf0eab13b5eefbbe888efbfb
Krayseer <krayseer@gmail.com>
2023-05-01 12:33:45 +0500

    Маркер стоянки теперь можно передвигать при его зажатии, также сделан обработчик, который обновляет местоположение маркера после его передвижения.

--- app/src/main/java/com/example/carchaser/MapsActivity.kt
+++ app/src/main/java/com/example/carchaser/MapsActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.carchaser.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -18,6 +19,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.DirectionsApi
@@ -30,7 +32,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var geocoder: Geocoder
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    private var isNight: Boolean = false
     private var markerIsAdd: Boolean = false
     private lateinit var position: LatLng
 
@@ -38,17 +43,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var btnCreateNote: Button
     private lateinit var btnDarkMode: Button
 
-    private var isNight: Boolean = false
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        geocoder = Geocoder(this)
 
+        geocoder = Geocoder(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         btnCreateNote = findViewById(R.id.btn_info)
@@ -62,12 +65,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         btnAddMarker.setOnClickListener {
-            if (!markerIsAdd){
-                markerIsAdd = true
-                addParkingPlace()
-                btnCreateNote.isEnabled = true
-                btnAddMarker.text = "Удалить"
-                btnAddMarker.isEnabled = false
+            if(ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED){
+                if (!markerIsAdd){
+                    markerIsAdd = true
+                    fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                            if (location != null) {
+                                val currentUserPosition = LatLng(location.latitude, location.longitude)
+                                position = currentUserPosition
+                                addParkingPlace(position)
+                            }
+                        }
+                    btnCreateNote.isEnabled = true
+                    btnAddMarker.text = "Удалить"
+                    btnAddMarker.isEnabled = false
+                }
             }
         }
 
@@ -96,12 +107,85 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.light_mode))
+
+        updateParkingPositionListener()
+        initCheckGeoPosition()
+        initMoveCamera()
+
         val arguments = intent.extras
         if (arguments != null) {
             position = arguments.get("coordinates") as LatLng
-            mMap.addMarker(MarkerOptions().position(position).title("Последняя стоянка"))
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 18f))
+            addParkingPlace(position)
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        initMoveCamera()
+    }
+
+    /**
+     * Функция построения маршрута от пользователя до маркера(автомобиля)
+     * НЕ РАБОТАЕТ из за того, что нужен платный API ключ.
+     */
+    private fun showRoute(origin: LatLng, destination: LatLng) {
+        val request = DirectionsApi
+            .newRequest(GeoApiContext
+                .Builder()
+                .apiKey("AIzaSyBg8K2t1NK9KDDD-rj-Zf-d-gqNaW3Xwb0")
+                .build())
+            .mode(TravelMode.WALKING)
+            .origin(com.google.maps.model.LatLng(origin.latitude, origin.longitude))
+            .destination(com.google.maps.model.LatLng(destination.latitude, destination.longitude))
+
+        val legs = request.await().routes[0].legs
+        for (leg in legs) {
+            for (step in leg.steps) {
+                for (point in step.polyline.decodePath()) {
+                    val pos = LatLng(point.lat, point.lng)
+                    mMap.addPolyline(PolylineOptions().add(pos).color(Color.BLUE).width(5f))
+                }
+            }
+        }
+    }
+
+    /**
+     * Функция добавления маркера на текущее местоположение пользователя
+     */
+    private fun addParkingPlace(markerPosition: LatLng) {
+        mMap.addMarker(MarkerOptions()
+            .position(markerPosition)
+            .title("Последняя стоянка")
+            .draggable(true))
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 18f))
+    }
+
+    /**
+     * Функция получения адреса по координатам
+     * @param coordinates координаты, по которым нужно получить адрес
+     * @return полученный адрес по координатам
+     */
+    private fun getAddressFromCoordinates(coordinates: LatLng): String? {
+        return geocoder
+            .getFromLocation(coordinates.latitude, coordinates.longitude, 1)
+            ?.get(0)
+            ?.getAddressLine(0)
+    }
+
+    /**
+     * Функция, которая при первом заходе в приложение запрашивает использование местоположения
+     */
+    private fun initCheckGeoPosition() {
+        if(ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), 0)
+    }
+
+    /**
+     * Функция, которая инициализирует местоположение камеры с учетом возможности использования
+     * геопозиции пользователя
+     */
+    private fun initMoveCamera() {
         if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED) {
             mMap.isMyLocationEnabled = true
             LocationServices
@@ -112,67 +196,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentUserPosition, 18f))
                     }
                 }
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), 0)
         }
     }
 
     /**
-     * Функция построения маршрута от пользователя до маркера(автомобиля)
+     * Функция устанавливает на карту слушатель, который обновляет глобальное значение position
+     * на текущее местоположение метки парковки
      */
-    private fun showRoute(origin: LatLng, destination: LatLng) {
-        val directionsApi = GeoApiContext.Builder()
-            .apiKey("AIzaSyBg8K2t1NK9KDDD-rj-Zf-d-gqNaW3Xwb0")
-            .build()
+    private fun updateParkingPositionListener() {
+        mMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+            override fun onMarkerDragStart(marker: Marker) { }
 
-        val request = DirectionsApi.newRequest(directionsApi)
-            .mode(TravelMode.WALKING)
-            .origin(com.google.maps.model.LatLng(origin.latitude, origin.longitude))
-            .destination(com.google.maps.model.LatLng(destination.latitude, destination.longitude))
+            override fun onMarkerDrag(marker: Marker) { }
 
-        val response = request.await()
-
-        val legs = response.routes[0].legs
-        for (leg in legs) {
-            for (step in leg.steps) {
-                for (point in step.polyline.decodePath()) {
-                    mMap.addPolyline(PolylineOptions()
-                            .add(LatLng(point.lat, point.lng))
-                            .color(Color.BLUE)
-                            .width(5f)
-                    )
-                }
+            override fun onMarkerDragEnd(marker: Marker) {
+                position = marker.position
             }
-        }
-    }
-
-    /**
-     * Функция добавления маркера на текущее местоположение пользователя (не работает, возможно
-     * из-за того, что нужен платный API ключ, но ошибка указывает именно на API ключ
-     */
-    private fun addParkingPlace() {
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED) {
-            LocationServices
-                .getFusedLocationProviderClient(this).lastLocation
-                .addOnSuccessListener { location: Location? ->
-                    if (location != null) {
-                        val currentUserPosition = LatLng(location.latitude, location.longitude)
-                        position = currentUserPosition
-
-                        mMap.clear()
-                        mMap.addMarker(MarkerOptions().position(currentUserPosition).title("Последняя стоянка"))
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentUserPosition, 18f))
-                    }
-                }
-        }
-    }
-
-    /**
-     * Функция получения адреса по координатам
-     */
-    private fun getAddressFromCoordinates(latLng: LatLng): String? {
-        val addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
-        return addresses?.get(0)?.getAddressLine(0)
+        })
     }
 
 }
